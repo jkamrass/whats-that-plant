df6c8c614f39d6e880c25b1a2cd24233690b3601
mchl-frlg <mchl.frlg@gmail.com>
2021-04-28 10:50:17 -0400

    basic quizzes built out

---
+++ src/components/quizzes/BeginnerScoreCard.js
@@ -0,0 +1,52 @@
+const BeginnerScoreCard = ({answer, score, numQuestions}) => {
+  
+  const writeGameMessage = (score) => {
+    switch (true){
+      case score === 5:
+        return 'great job! you\'re ready for our gardener\'s quiz!'
+      case score === 4:
+        return 'well done!'
+      case score === 3:
+        return 'you\'re getting the hang of it!'
+      case score ===2:
+        return 'maybe a little more practice will help'
+      default:
+        return 'better luck next time, champ'
+    }
+  }
+  
+  if (numQuestions < 5) {
+    return (
+      <>
+        <div className = 'row'>
+          <div className='col-sm-8 offset-md-2'>
+            <h3>can you identify...</h3>
+            <h1>{answer}</h1>
+          </div>
+        </div>
+        
+        <div className = 'row'>
+          <div className='col-sm-8 offset-md-2'>
+            <h3>your score</h3>
+            <h1>{score}/{numQuestions}</h1>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  return (
+    <>
+      <div className = 'row'>
+        <div className='col-sm-8 offset-md-2'>
+          <h3>{writeGameMessage(score)}</h3>
+          <h1>Your score:</h1>
+          <h1>{score}/{numQuestions}</h1>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default BeginnerScoreCard;
---
+++ src/components/quizzes/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData = {
+  plants: [
+    { 
+      name: 'Saguaro Cactus',
+      image: 'https://debraleebaldwin.com/wp-content/uploads/Saguaro-cactus.jpg'
+    },
+    { 
+      name: 'Boston Fern',
+      image: 'https://i2.wp.com/www.gardeningchannel.com/wp-content/uploads/2021/01/boston-ferns.jpg?resize=750%2C514&ssl=1'
+    },
+    { 
+      name: 'Lemon Tree',
+      image: 'https://i.guim.co.uk/img/media/6a7b34f889f80e543b5e43c319d22ee55b4b5433/0_245_3264_1958/master/3264.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=9ca73aff1ca6087935d2b4cda0bf5a5c'
+    },
+    { 
+      name: 'Poison Oak',
+      image: 'https://www.almanac.com/sites/default/files/image_nodes/poison-oak-identify-treat.jpg'
+    },
+    { 
+      name: 'Bromeliad',
+      image: 'https://www.gardeningknowhow.com/wp-content/uploads/2012/03/bromeliads-1.jpg'
+    },
+  ],
+  currentScore: 0,
+  questionsCompleted: 0
+}
