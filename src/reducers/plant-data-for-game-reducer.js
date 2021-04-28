117f63e282f1f92b0bc319ee6dd41e2021065208
mchl-frlg <mchl.frlg@gmail.com>
2021-04-28 14:43:04 -0400

    with answers cycle

--- src/reducers/plant-data-for-game-reducer.js
+++ src/reducers/plant-data-for-game-reducer.js
@@ -2,10 +2,21 @@ import {FETCH_TREFLE_GAME_INFORMATION, UPDATE_ANSWER} from "../actions/index";
 
 
 const plantDataForGameReducer = (state = [], action) => {
-  const randomValue = Math.floor(Math.random()*5);
+  const randomValue = (param) => Math.floor(Math.random()*param);
+
+  const imageRandomizer = (array, numImages) => {
+    const plantsForDisplay = [];
+    const arrayCopy = [...array]
+    for (let i = 0; i < numImages; i++) {
+      
+      plantsForDisplay.push(arrayCopy.splice(randomValue(arrayCopy.length), 1))
+    }
+    return plantsForDisplay.flat();
+  }
 
   switch (action.type) {
     case FETCH_TREFLE_GAME_INFORMATION:
+      debugger;
       const plantInfoForGame = action.payload.data.data.map(plant=> {
           return {
             id: plant.id,
@@ -17,17 +28,25 @@ const plantDataForGameReducer = (state = [], action) => {
             }
           }
       })
+
+      const plantsDisplayed = imageRandomizer(plantInfoForGame, 5);
+
       const gameData = {
         plants: plantInfoForGame,
-        answer: plantInfoForGame[randomValue].plantData.commonName,
+        plantsDisplayed: plantsDisplayed,
+        answer: plantsDisplayed[randomValue(5)].plantData.commonName,
         score: 0,
         numQuestions: 0
       }
+      debugger;
       return gameData;
     
     case UPDATE_ANSWER:
+      const plantDisplay = imageRandomizer(state.plants, 5);
+    
       const newTurn = {
-        answer: state.plants[randomValue].plantData.commonName,
+        answer: plantDisplay[randomValue(5)].plantData.commonName,
+        plantsDisplayed: plantDisplay,
         score: state.score,
         numQuestions: state.numQuestions + 1
       }
